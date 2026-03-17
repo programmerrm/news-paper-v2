5e63f9090f367adb1a0260f480107bc1d3ad5a6e
shafiqahmad81 <shafiqahmad081@gmail.com>
2026-03-17 13:22:35 +0600

    update

---
+++ app/About-us/page.tsx
@@ -0,0 +1,13 @@
+
+export default function AboutUs() {
+    return (
+        <>
+            <section className="py-5 sm:py-10">
+                <div className="container">
+                    <h2 className="text-2xl font-bold">আমাদের সম্পর্কে</h2>
+                    
+                </div>
+            </section>
+        </>
+    );
+}
